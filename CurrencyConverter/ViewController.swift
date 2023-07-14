3257604a67a03bfeb0516a9b8ebe041184e64dab
fulyaaa <fulyaakan@outlook.com>
2023-07-14 17:37:05 +0300

    Request & Session

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //api: bir sunucudan veri çekme
+        //json: obje gösterme formatı
     }
-
-
+    
+    @IBAction func getRatesClicked(_ sender: Any) {
+        
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=4a990ae1cc0ef5a920e4c7e9eeb1123c")
+        
+        let session = URLSession.shared
+        
+        //Closure
+        let task = session.dataTask(with: url!) {(data, response, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                
+                if data != nil {
+                    
+                }
+                
+            }
+                
+        }
+        
+        task.resume()
+        
+    }
+    
 }
 
